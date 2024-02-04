5e1ec7e38ce0c48dcf16e6c4dc9cf5315952bbe5
Diego Carmona <diegocarmona@my.whccd.edu>
2024-02-03 17:51:10 -0800

    Finished Comment Section

--- COMP003A.LectureActivity4/Program.cs
+++ COMP003A.LectureActivity4/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* 
+ * Author: Diego Carmona
+ * Course: COMP-003A
+ * Purpose: To gain notes and reliable foundation for the usage and writing of loops within C#
+ */
 
 namespace COMP003A.LectureActivity4
 {
@@ -10,6 +15,7 @@ namespace COMP003A.LectureActivity4
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
